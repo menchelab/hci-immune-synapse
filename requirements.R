f767ffe0073a35a765d26ee5241701b5e293183c
Loan Vulliard <lvulliard@cemm.oeaw.ac.at>
2021-02-22 16:13:05 +0100

    Complete 3D analysis and clean code for submission

--- requirements.R
+++ requirements.R
@@ -1,10 +1,14 @@
 # Set R repository to a fixed backup for repository
-options(repos = list(CRAN = 'http://mran.revolutionanalytics.com/snapshot/2019-01-06/'))
+options(repos = list(CRAN = 'http://mran.revolutionanalytics.com/snapshot/2021-01-01/'))
 
 # Install packages not present in the Docker image by default
+install.packages("sp")
+install.packages("ggplot2")
 install.packages("extrafont")
 install.packages("heatmaply")
 install.packages("ggrepel")
 install.packages("ggpubr")
+install.packages("igraph")
+install.packages("dplyr")
 install.packages("reticulate")
 install.packages("robustbase")
